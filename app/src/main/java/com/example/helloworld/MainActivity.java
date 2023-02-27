48b92eb4a4d45ea924d6c50d79169ce5d7ba096c
nnnidooo <nadi3252@stud.kea.dk>
2023-02-27 09:52:29 +0100

    A & B

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "App up and running");
+        super.onPause();
+        Log.d("MainActivity", "On pause");
+        super.onResume();
+        Log.d("MainActivity", "On resume");
     }
 }
